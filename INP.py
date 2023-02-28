b9fd6b49dc16d5c74ffe27214096890c9973b8db
PRINCE-BRAND <125208820+PRINCE-BRAND@users.noreply.github.com>
2023-02-28 16:46:12 +0545

    Update INP.py

--- INP.py
+++ INP.py
@@ -1,2 +1,30 @@
-from INP1 import INP1
-INP1.INP1()
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from INP1 import INP1
+ 
+        INP1()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from INP32 import INP32
+ 
+ 
+        INP32()
